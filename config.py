5f2e7f4981002b59b94508288cd06a837e1c4684
AKITO <dipeshkoli86@gmail.com>
2024-10-27 08:41:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
-OWNER_ID = os.getenv("OWNER_ID",1238234357)
+OWNER_ID = os.getenv("OWNER_ID",6325887601)
 HANDLER = ["~", ".","!","?","@","$"]
 BARATH = "https://te.legra.ph/file/6a839cecad51014a32254.mp4"
-GROUP_ID = os.getenv("GROUP_ID", "-1002119409366")
-SOURCE = "https://github.com/SIAmKira/PyroX-V2"
-LOG_CHAT = -1002119409366
+GROUP_ID = os.getenv("GROUP_ID", "-1002109988264")
+SOURCE = "https://t.me/THE_AKITO"
+LOG_CHAT = -1002109988264
+
